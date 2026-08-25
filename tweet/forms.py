7e46e81099084a5c09806a51dc76a44b680fcd06
Ayush Waghmare <ayushwaghmare009@gmail.com>
2026-08-25 11:13:39 +0530

    Initial commit of Tweetbar project

---
+++ tweet/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Tweet
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ['text', 'photo']
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Remove noisy default help text
+        self.fields['username'].help_text = ''
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+
+        # Apply Bootstrap styling
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
---
+++ tweet/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib import messages
+from django.db.models import Q
+from .models import Tweet
+from .forms import TweetForm, UserRegistrationForm
+
+
+def tweet_list(request):
+    """Displays all tweets or filters them based on a search query."""
+    query = request.GET.get('search')
+    if query:
+        tweets = Tweet.objects.filter(
+            Q(text__icontains=query) | Q(user__username__icontains=query)
+        ).order_by('-created_at')
+    else:
+        tweets = Tweet.objects.all().order_by('-created_at')
+
+    return render(request, 'tweet/tweet_list.html', {'tweets': tweets})
+
+
+@login_required
+def tweet_create(request):
+    """Handles tweet creation for logged-in users."""
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            messages.success(request, 'Tweet posted successfully!')
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+
+    return render(request, 'tweet/tweet_form.html', {'form': form})
+
+
+@login_required
+def tweet_edit(request, tweet_id):
+    """Allows a user to edit their own tweet."""
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Tweet updated successfully!')
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+
+    return render(request, 'tweet/tweet_form.html', {'form': form})
+
+
+@login_required
+def tweet_delete(request, tweet_id):
+    """Allows a user to delete their own tweet."""
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        tweet.delete()
+        messages.warning(request, 'Tweet deleted.')
+        return redirect('tweet_list')
+
+    return render(request, 'tweet/tweet_confirm_delete.html', {'tweet': tweet})
+
+
+@login_required
+def tweet_like(request, tweet_id):
+    """Toggles like/unlike on a tweet."""
+    tweet = get_object_or_404(Tweet, pk=tweet_id)
+    if tweet.likes.filter(id=request.user.id).exists():
+        tweet.likes.remove(request.user)
+    else:
+        tweet.likes.add(request.user)
+    return redirect('tweet_list')
+
+
+def register(request):
+    """Handles new user registration and logs them in immediately."""
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Welcome to Tweetbar, {user.username}!')
+            return redirect('tweet_list')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
