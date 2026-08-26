55c29fee5d4069d155957b7c860fa570ab13f0f5
Ayush Waghmare <ayushwaghmare009@gmail.com>
2026-08-26 19:54:10 +0530

    Add Bootstrap JS and WhiteNoise static file handling

--- ayushheadq/settings.py
+++ ayushheadq/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,3 +134,4 @@ LOGIN_REDIRECT_URL = 'tweet_list'
 LOGOUT_REDIRECT_URL= 'tweet_list'
 LOGIN_URL = 'login'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
