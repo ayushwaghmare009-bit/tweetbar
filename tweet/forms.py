fc3a0df6324b7a7ac5b220c88acfec87e4e210d2
Ayush Waghmare <ayushwaghmare009@gmail.com>
2026-08-27 16:58:29 +0530

    Fix ModelForm import typo in forms.py

--- tweet/forms.py
+++ tweet/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Tweet
-from .models import Profile,models
+from .models import Profile
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
@@ -11,7 +11,7 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
-class ProfileUpdateForm(models.ModelForm):
+class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
