2d8d117c1e3b7fc37cad622758072518edc7bb30
Ayush Waghmare <ayushwaghmare009@gmail.com>
2026-08-28 13:09:00 +0530

    Configure PostgreSQL database with dj-database-url

--- ayushheadq/settings.py
+++ ayushheadq/settings.py
@@ -81,13 +81,15 @@ WSGI_APPLICATION = 'ayushheadq.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+# Replace default DATABASES setting with this:
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
